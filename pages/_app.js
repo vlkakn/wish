c96bdc8054041d2a29a0cb209a12f1f1684b8437
vlkakn <v_akin@hotmail.com>
2022-02-18 18:02:28 +0300

    Auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/global.css'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Auth0Provider
+      domain="dev-f8dpi8va.us.auth0.com"
+      clientId="LBksNy2QGfJGrsqEeSDoyUUsvYk5pQoE"
+      redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import Layout from '../components/layout'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function HomePage() {
-  return <Layout>Ana Sayfa içeriği</Layout>
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0()
+
+  return (
+    <Layout>
+      <p>Ana Sayfa içeriği</p>
+      {isAuthenticated ? 'yes ' + user.name : 'no'}
+      {console.log(user)}
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+      <button onClick={() => logout({ returnTo: process.env.NEXT_PUBLIC_URL })}>
+        Log Out
+      </button>
+    </Layout>
+  )
 }
 
 export async function getStaticProps() {
